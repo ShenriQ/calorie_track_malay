d17c5acec3b40fc6eaff723d3b9eac5fc0e8e1ca
dev <dev@dev.com>
2021-04-05 21:34:27 +0100

    font family changed  to Plus Jakarta Sans

--- app_calorie_track_malay/App.js
+++ app_calorie_track_malay/App.js
@@ -4,9 +4,24 @@ import { Provider } from 'react-redux'
 import store from './src/redux/store';
 import MemberRoute from './src/routes/Member';
 import GuestRoute from './src/routes/Guest';
+import {
+  setCustomView,
+  setCustomTextInput,
+  setCustomText,
+  setCustomImage,
+  setCustomTouchableOpacity
+} from 'react-native-global-props';
+
+const customTextProps = {
+  style: {
+    fontFamily: 'PlusJakartaSans',
+  }
+};
+
+setCustomText(customTextProps);
 
 const App = (props) => {
-    const [isLogged, SetIsLogged] = useState(false)
+    const [isLogged, SetIsLogged] = useState(true)
 
     return (
       <Provider store={store}>
